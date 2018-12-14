c017d20551feaeb17190a8f5c72de4d78f374cd4
Vince Salvino <salvino@coderedcorp.com>
2018-12-14 15:00:51 -0500

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -1,4 +1,6 @@
 from coderedcms.blocks import BaseBlock
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 from pygments import highlight
 from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.formatters import HtmlFormatter
